0303d3044948e6905c46a7d7bf0cee592d94e9b9
juliocabrera820 <juliocabrera820@gmail.com>
2019-04-04 18:02:28 -0600

    Sin funciones getter y setter para el peso

--- src/grafo/arista.java
+++ src/grafo/arista.java
@@ -12,17 +12,19 @@ public class arista {
     private int x1, y1, x2, y2;
     private String nombre;
 
-    public arista(int x1, int y1, int x2, int y2,String nombre) {
+    public arista(int x1, int y1, int x2, int y2/*,String nombre*/) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        this.nombre = nombre;
+        //this.nombre = nombre;
     }
 
     public void pintar(Graphics g) {
 
         g.drawLine(x1, y1, x2, y2);
+        
+        /*
         
         if (x1 > x2 && y1 > y2) {
             
@@ -40,6 +42,8 @@ public class arista {
             
             g.drawString(nombre, x2 - Math.abs((x1 - x2) / 2), y1 - Math.abs((y1 - y2) / 2));
         }
+
+        */
          
     }
 
